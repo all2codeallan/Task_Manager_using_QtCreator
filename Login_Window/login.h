e5a0e1273b6b84191081c1112d30b18aca23cd15
all2codeallan <140888932+all2codeallan@users.noreply.github.com>
2024-03-22 21:56:49 +0530

    Create login.h

---
+++ Login_Window/login.h
@@ -0,0 +1,29 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <QDialog>
+
+namespace Ui {
+class Login;
+}
+
+class Login : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Login(QWidget *parent = nullptr);
+    ~Login();
+    void applyStylesheets();
+
+signals:
+    void loginSuccessful();
+
+private slots:
+    void on_loginButton_clicked();
+
+private:
+    Ui::Login *ui;
+};
+
+#endif // LOGIN_H
